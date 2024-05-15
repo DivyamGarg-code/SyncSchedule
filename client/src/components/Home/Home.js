15e755fc2f5dd8b30a6d77c790e1347f945b05c1
Divyam Garg <dvm.garg26@gmail.com>
2024-05-16 02:30:29 +0530

    fixed bugs

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import homeIcon from '../../images/home_icon.png'
 
 function Home() {
   return (
-    <div className='text-2xl font-bold'>Welcome to the homepage</div>
+    <div className='h-[600px] flex flex-col items-center justify-start'>
+      <div className='w-[90vw] h-[500px] bg-purple-600 rounded-lg relative flex flex-col items-center justify-start gap'>
+        <span className='font-bold text-2xl text-center'>Unified Timetable Hub: Solve Time Clashes and Sync Schedules</span>
+        {/* <span>Welcome To the Time Table</span> */}
+        <img src={homeIcon} alt="error" className='pb-3 w-[270px] absolute left-1/2 -translate-x-1/2 top-[330px]' />
+      </div>
+    </div>
   )
 }
 
